7d9c929ede5c84624de0a9654d8a4b591d2abbe5
jamesli <jamespeilunli@gmail.com>
2025-02-16 22:46:21 -0800

    move and remove tiles

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,14 @@ const boardMarginTop = boardBoundingRect.top + window.scrollY;
 const boardMarginLeft = boardBoundingRect.left + window.scrollX;
 
 class Tile {
+  i: number;
+  j: number;
   element: HTMLDivElement;
 
   constructor(value: number, i: number, j: number) {
+    this.i = i;
+    this.j = j;
+
     this.element = document.createElement("div");
     this.element.classList.add("tile");
     this.element.style.backgroundColor = "black";
@@ -22,17 +27,47 @@ class Tile {
     this.element.innerText = value.toString();
     document.getElementById("board")?.appendChild(this.element);
   }
+
+  destroy() {
+    this.element.remove();
+  }
+
+  moveTo(i:number, j: number) {
+    this.i = i;
+    this.j = j;
+    this.element.style.left = `${boardMarginLeft + tileMargin + j * (tileSize + tileMargin)}px`;
+    this.element.style.top = `${boardMarginTop + tileMargin + i * (tileSize + tileMargin)}px`;
+  }
 }
 
 class Board {
-  board: Tile[];
+  tiles: Tile[];
 
   constructor() {
-    this.board = [];
+    this.tiles = [];
   }
 
   createTile(value: number, i: number, j: number) {
-    this.board.push(new Tile(value, i, j));
+    this.tiles.push(new Tile(value, i, j));
+  }
+
+  removeTile(i: number, j: number) {
+    this.tiles.forEach((tile, index) => {
+      if (tile.i === i && tile.j === j) {
+        tile.destroy();
+        this.tiles.splice(index, 1);
+        return;
+      }
+    })
+  }
+
+  moveTile(fromI:number, fromJ:number, toI:number, toJ:number) {
+    this.tiles.forEach((tile, index) => {
+      if (tile.i === fromI && tile.j === fromJ) {
+        tile.moveTo(toI, toJ);
+        return;
+      }
+    })
   }
 }
 
@@ -43,3 +78,8 @@ for (let i = 0; i < 4; i++) {
   }
 }
 
+console.log(board.tiles);
+board.removeTile(2, 2);
+console.log(board.tiles);
+board.moveTile(3, 3, 2, 2);
+console.log(board.tiles);
