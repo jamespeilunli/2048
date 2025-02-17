a40ee77d8514ccf60a9d947288a69d45f9c98cf0
jamesli <jamespeilunli@gmail.com>
2025-02-16 22:33:22 -0800

    add organized tiles

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,45 @@
+// note: most of the things inside parseInt are strings with "px" suffix
 const styles = getComputedStyle(document.documentElement);
-const tilesPerRow = styles.getPropertyValue('--tiles-per-row');
-const tileSize = styles.getPropertyValue('--tile-size');
-const tileMargin = styles.getPropertyValue('--tile-margin');
-const tileFontSize = styles.getPropertyValue('--tile-font-size');
+const tilesPerRow = parseInt(styles.getPropertyValue('--tiles-per-row'));
+const tileSize = parseInt(styles.getPropertyValue('--tile-size'));
+const tileMargin = parseInt(styles.getPropertyValue('--tile-margin'));
+const tileFontSize = parseInt(styles.getPropertyValue('--tile-font-size'));
+
+const boardBoundingRect = document.getElementById("board")!.getBoundingClientRect();
+const boardMarginTop = boardBoundingRect.top + window.scrollY;
+const boardMarginLeft = boardBoundingRect.left + window.scrollX;
 
 class Tile {
   element: HTMLDivElement;
 
-  constructor(value: number) {
+  constructor(value: number, i: number, j: number) {
     this.element = document.createElement("div");
     this.element.classList.add("tile");
     this.element.style.backgroundColor = "black";
     this.element.style.color = "pink";
-    this.element.innerText = `${value}`;
+    this.element.style.left = `${boardMarginLeft + tileMargin + j * (tileSize + tileMargin)}px`;
+    this.element.style.top = `${boardMarginTop + tileMargin + i * (tileSize + tileMargin)}px`;
+    this.element.innerText = value.toString();
     document.getElementById("board")?.appendChild(this.element);
   }
 }
 
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
-new Tile(4);
+class Board {
+  board: Tile[];
+
+  constructor() {
+    this.board = [];
+  }
+
+  createTile(value: number, i: number, j: number) {
+    this.board.push(new Tile(value, i, j));
+  }
+}
+
+const board = new Board();
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 4; j++) {
+    if (i === j) board.createTile(2, i, j);
+  }
+}
+
