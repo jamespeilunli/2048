e128b424096e696efcbf13ee2004b0ff0539745b
jamesli <jamespeilunli@gmail.com>
2025-02-16 18:27:41 -0800

    basic tile class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
-console.log("Hello world!");
+const styles = getComputedStyle(document.documentElement);
+const tilesPerRow = styles.getPropertyValue('--tiles-per-row');
+const tileSize = styles.getPropertyValue('--tile-size');
+const tileMargin = styles.getPropertyValue('--tile-margin');
+const tileFontSize = styles.getPropertyValue('--tile-font-size');
 
+class Tile {
+  element: HTMLDivElement;
+
+  constructor(value: number) {
+    this.element = document.createElement("div");
+    this.element.classList.add("tile");
+    this.element.style.backgroundColor = "black";
+    this.element.style.color = "pink";
+    this.element.innerText = `${value}`;
+    document.getElementById("board")?.appendChild(this.element);
+  }
+}
+
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
+new Tile(4);
